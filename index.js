a1c8da725861962d2d190628a5fa44c1fd749509
Abdullah2599 <abdullahcloudupload@gmail.com>
2024-10-12 09:57:59 +0500

    added folder and files work on basics routes

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs')
+
+app.get('/',function(req,res){
+    res.render('home')
+})
+app.get('/',function(req,res){
+    res.status(200).json({message:"Home Page"})
+})
+
+app.get('/about',function(req,res){
+    res.status(200).json({message:"About Page"})
+})
+
 
 app.listen(8080,function(){
     console.log("server started running on port 8080 ")
